7dd1f232c0e4277abaa8002fe2e2abfa5117e335
Vincenzo0426 <113822102+Vincenzo0426@users.noreply.github.com>
2022-12-19 23:38:25 +0800

    commit_1219

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -8,113 +9,138 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
+   @override
+   Widget build(BuildContext context){
+      return const MaterialApp(
+         debugShowCheckedModeBanner: false,
+         home: MyHomePage(),
+      );
+   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+   const MyHomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme
-          .of(context)
-          .textTheme
-          .headline4!,
-      child: Container(
-        color: Colors.white,
-        alignment: Alignment.center,
-        child: const Text('Home Page'),
-      ),
-    );
-  }
-  _MyHomePageState createState() => _MyHomePageState();
+   @override
+   Widget build(BuildContext context) {
+      return DefaultTextStyle(
+         style: Theme
+            .of(context)
+            .textTheme
+            .headline4!,
+         child: Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: const Text('Home Page'),
+         ),
+      );
+   }
+   _MyHomePageState createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-  bool takeOff = false;
+   bool takeOff = false;
+   bool fuck1 = false;
+   bool fuck2 = false;
+   bool fuck3 = false;
+   bool fuck4 = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('選擇困難小飛機'),
-      ),
-      body: SizedBox(
-        child: Stack(
-          children: <Widget>[
-            AnimatedPositioned(
-              top: takeOff ? 20.0 : 200.0,
-              bottom: takeOff ? 40.0 : 200.0,
-              left: takeOff ? -300.0 : 162.0,
-              right: takeOff ? 20.0 : 162.0,
-              duration: const Duration(seconds: 4),
-              curve: Curves.fastLinearToSlowEaseIn,
-              child: const Icon(
-                  Icons.airplanemode_active,
-                  color: Colors.blueGrey,
-                  size: 70.0
-              ),
+   @override
+   Widget build(BuildContext context) {
+      return Scaffold(
+         appBar: AppBar(
+            title: const Text('選擇困難小飛機'),
+         ),
+         body: SizedBox(
+            child: Stack(
+               children: <Widget>[
+                  AnimatedPositioned(
+                     top: fuck1 && takeOff ?  -250 : 100.0,
+                     bottom: fuck2 && takeOff ? -300 : 200.0,
+                     left: fuck3 && takeOff ? -100.0 : 162.0,
+                     right: fuck4 && takeOff ? -100 : 162.0,
+                     duration: const Duration(seconds: 4),
+                     curve: Curves.fastLinearToSlowEaseIn,
+                     child: const Icon(
+                        Icons.airplanemode_active,
+                        color: Colors.blueGrey,
+                        size: 70.0
+                     ),
+                  ),
+
+                  //換頁看結果按鈕
+                  Container(
+                     alignment: Alignment.bottomCenter,
+                     child: ElevatedButton(
+                        child: const Text('看結果'),
+                        onPressed: () {
+                           Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const CongratulationPage()),
+                           );
+                        }
+                     ),
+                  ),
+
+                  //飛機出發按鈕
+                  Center(
+                     child: ElevatedButton.icon(
+                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue), foregroundColor: MaterialStateProperty.all(Colors.white)),
+                        icon: const Icon(Icons.airplanemode_active, color: Colors.white),
+                        label: const Text('出發!!!'),
+                        onPressed: () => {
+                           setState(() {
+                              takeOff = !takeOff;
+                              if(takeOff){
+                              int random = Random().nextInt(4);
+                                 if (random==0) {
+                                    fuck1=true;
+                                 }
+                                 if (random==1) {
+                                    fuck2=true;
+                                 }
+                                 if (random==2) {
+                                    fuck3=true;
+                                 }
+                                 if (random==3) {
+                                    fuck4=true;
+                                 }
+                              }
+
+                              if(!takeOff) {
+                                 fuck1=false;
+                                 fuck2=false;
+                                 fuck3=false;
+                                 fuck4=false;
+                              }
+                           })
+                        },
+                     ),
+                  )
+               ],
             ),
-
-            //換頁看結果按鈕
-            Container(
-              alignment: Alignment.bottomCenter,
-              child: ElevatedButton(
-                  child: const Text('看結果'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const CongratulationPage()),
-                    );
-                  }
-              ),
-            ),
-
-            //飛機出發按鈕
-            Center(
-              child: ElevatedButton.icon(
-                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue), foregroundColor: MaterialStateProperty.all(Colors.white)),
-                icon: const Icon(Icons.airplanemode_active, color: Colors.white),
-                label: const Text('出發!!!'),
-                onPressed: () => {
-                  setState(() {
-                    takeOff = !takeOff;
-                  })
-                },
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+         ),
+      );
+   }
 }
 
 class CongratulationPage extends StatelessWidget {
-  const CongratulationPage({super.key});
+   const CongratulationPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('恭喜，您抽中了' + ''),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('回上頁'),
-        ),
-      ),
-    );
-  }
+   @override
+   Widget build(BuildContext context) {
+      return Scaffold(
+         appBar: AppBar(
+            title: const Text('恭喜，您抽中了' + ''),
+         ),
+         body: Center(
+            child: ElevatedButton(
+               onPressed: () {
+                  Navigator.pop(context);
+               },
+               child: const Text('回上頁'),
+            ),
+         ),
+      );
+   }
 }
